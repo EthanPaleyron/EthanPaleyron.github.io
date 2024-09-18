b686496cc109b272480a61c9243c363d7d5e85e6
EthanPaleyron <epaleyron@edenschool.fr>
2024-09-18 19:09:11 +0200

    resolve bug bg music

--- js/music.js
+++ js/music.js
@@ -11,26 +11,35 @@ function playAudioOnce(event) {
 
 document.querySelector("body").addEventListener("click", playAudioOnce);
 
+let isFading = false;
+
 document.querySelector("#volume").addEventListener("click", (e) => {
-  e.stopPropagation(); // Empêche l'événement de se propager au body
+  e.stopPropagation();
+
+  if (isFading) return;
+
   if (audio.volume !== 0) {
     icon.src = "public/icon/volume-xmark.svg";
+    isFading = true;
     let volumeFadeOut = setInterval(() => {
       if (audio.volume > 0.05) {
         audio.volume -= 0.05;
       } else {
         audio.volume = 0;
         clearInterval(volumeFadeOut);
+        isFading = false;
       }
     }, 50);
   } else {
     icon.src = "public/icon/volume-high.svg";
+    isFading = true;
     let volumeFadeIn = setInterval(() => {
       if (audio.volume < 0.95) {
         audio.volume += 0.05;
       } else {
         audio.volume = 1;
         clearInterval(volumeFadeIn);
+        isFading = false;
       }
     }, 50);
   }
