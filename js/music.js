2d64d552e2bb3865c41a3042e33e0e316dd16b7b
EthanPaleyron <epaleyron@edenschool.fr>
2024-12-29 05:59:45 +0100

    fix

--- js/music.js
+++ js/music.js
@@ -3,17 +3,23 @@ const closePopup = document.querySelector("#closePopup");
 const volume = document.querySelector("#volume");
 const icon = document.querySelector("#volume > img");
 
-closePopup.addEventListener("click", () => {
-  audio.play(); // Assurez-vous que l'audio démarre avec une interaction utilisateur
-});
-
 let isFading = false;
-let isPlay = true;
+let isPlaying = false;
+
+closePopup.addEventListener("click", () => {
+  if (!isPlaying) {
+    audio.play();
+    isPlaying = true;
+    icon.src = "public/icon/volume-high.svg";
+    icon.alt = "Couper la musique";
+    volume.title = "Couper la musique";
+  }
+});
 
 volume.addEventListener("click", (e) => {
   if (isFading) return;
 
-  if (isPlay) {
+  if (isPlaying) {
     icon.src = "public/icon/volume-xmark.svg";
     icon.alt = "Remettre la musique";
     volume.title = "Remettre la musique";
@@ -23,20 +29,17 @@ volume.addEventListener("click", (e) => {
         audio.volume -= 0.05;
       } else {
         audio.volume = 0;
-        audio.pause(); // Met en pause l'audio
+        audio.pause();
+        isPlaying = false;
         clearInterval(volumeFadeOut);
-        isPlay = false;
         isFading = false;
       }
     }, 50);
   } else {
-    // Relance la musique uniquement si l'audio est en pause
-    if (audio.paused) {
-      audio.play();
-      icon.src = "public/icon/volume-high.svg";
-      icon.alt = "Couper la musique";
-      volume.title = "Couper la musique";
-    }
+    audio.play();
+    icon.src = "public/icon/volume-high.svg";
+    icon.alt = "Couper la musique";
+    volume.title = "Couper la musique";
     isFading = true;
     let volumeFadeIn = setInterval(() => {
       if (audio.volume < 0.95) {
@@ -44,7 +47,7 @@ volume.addEventListener("click", (e) => {
       } else {
         audio.volume = 1;
         clearInterval(volumeFadeIn);
-        isPlay = true;
+        isPlaying = true;
         isFading = false;
       }
     }, 50);
